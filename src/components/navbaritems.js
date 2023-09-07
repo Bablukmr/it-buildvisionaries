297b4fd30a9d69f135774fcbf885d7b21e9811e3
Bablukmr <bablukmr800@gmail.com>
2023-09-07 17:53:58 +0530

    navitem

---
+++ src/components/navbaritems.js
@@ -0,0 +1,43 @@
+const navitem = [
+  {
+    title: Service,
+    url: "/",
+    subitem: [
+      { title: service1, url: "/" },
+      { title: service2, url: "/" },
+      { title: service1, url: "/" ,subitem: [
+        { title: HETW, url: "/" },
+        { title: RTHHT, url: "/" },
+        { title: TRHT, url: "/" ,},
+        { title: KU8U7, url: "/" },
+        { title: TYTYWE, url: "/" },
+      ],},
+      { title: service3, url: "/" },
+      { title: service4, url: "/" },
+    ],
+  },
+  {
+    title: contact,
+    url: "/",
+    subitem: [
+      { title: service1, url: "/" },
+      { title: service2, url: "/" },
+      { title: service1, url: "/" },
+      { title: service3, url: "/" },
+      { title: service4, url: "/" },
+    ],
+  },
+  { title: avout, url: "/" },
+  { title: career, url: "/" },
+  {
+    title: join,
+    url: "/",
+    subitem: [
+      { title: service1, url: "/" },
+      { title: service2, url: "/" },
+      { title: service1, url: "/" },
+      { title: service3, url: "/" },
+      { title: service4, url: "/" },
+    ],
+  },
+];
