c8110f342792a7afbd9ce636c3ee79959b231a4c
Bablukmr <bablukmr800@gmail.com>
2023-09-07 18:14:03 +0530

    Link

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,25 @@
 "use client"
 import Image from "next/image";
-import img from '../assets/iconpng.png'
+import img from '../../assets/iconpng.png'
 import Link from "next/link";
-import './navbar.css'
+import Styles from './Navebar.modules.css'
+import {navitem} from './navbaritems'
+
 
 export default function Navbar() {
+  console.log(navitem);
   return (
-    <nav className="navbar mt-0 rounded-sm  flex justify-between">
+    <nav className={`${Styles.navbar} mt-0 rounded-sm  flex justify-between`}>
       <div >
      <Link href={'/'} className="flex items-center"><Image src={img} alt="Logo" width={80}/><h1 className="text-blue-800 text-lg font-bold ">BUILD<span className="text-amber-300">VISIONARY</span></h1></Link> 
       </div>
       <div >
-        <ul className="menu m-7 text-lg hidden sm:flex md:flex">
-          <Link href={'/'}><li>Home</li></Link>
-          <Link href={'/'}><li>Home</li></Link>
-          <Link href={'/'}><li>Contact</li></Link>
-          <Link href={'/'}><li>Home</li></Link>
-          <Link href={'/'}><li>About</li></Link>
+        <ul className={`${Styles.menu} m-7 text-lg hidden sm:flex md:flex`}>
+        {
+          navitem.map((item,index)=>
+          {return( <li key={index}>{item.title}</li>)}
+          )
+        }
         </ul>
       </div>
       
---
+++ src/components/Navbar/navbaritems.js
@@ -0,0 +1,47 @@
+export const navitem = [
+  {
+    title: "Service",
+    url: "/",
+    subitem: [
+      { title: "Service 1", url: "/" },
+      { title: "Service 2", url: "/" },
+      {
+        title: "Service 1",
+        url: "/",
+        subitem: [
+          { title: "HETW", url: "/" },
+          { title: "RTHHT", url: "/" },
+          { title: "TRHT", url: "/" },
+          { title: "KU8U7", url: "/" },
+          { title: "TYTYWE", url: "/" },
+        ],
+      },
+      { title: "Service 3", url: "/" },
+      { title: "Service 4", url: "/" },
+    ],
+  },
+  {
+    title: "Contact",
+    url: "/",
+    subitem: [
+      { title: "Service 1", url: "/" },
+      { title: "Service 2", url: "/" },
+      { title: "Service 1", url: "/" },
+      { title: "Service 3", url: "/" },
+      { title: "Service 4", url: "/" },
+    ],
+  },
+  { title: "About", url: "/" },
+  { title: "Career", url: "/" },
+  {
+    title: "Join",
+    url: "/",
+    subitem: [
+      { title: "Service 1", url: "/" },
+      { title: "Service 2", url: "/" },
+      { title: "Service 1", url: "/" },
+      { title: "Service 3", url: "/" },
+      { title: "Service 4", url: "/" },
+    ],
+  },
+];
