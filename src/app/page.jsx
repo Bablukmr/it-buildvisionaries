454d4e4361b7f3d9e51cd0edab8641173ef43c13
Bablukmr <bablukmr800@gmail.com>
2023-09-10 17:55:34 +0530

    Carouse

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,7 @@
-import Carousels from "../components/Carousel/Carousel";
 
 export default function Home() {
   return (
    <div>
-    <div ><Carousels /></div>
 
  <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod ullam expedita deserunt quo sit nobis assumenda inventore illo aliquid! Placeat vero consectetur maiores assumenda doloremque harum quasi unde ab ex?</h1>
    </div>
